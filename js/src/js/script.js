8b11a16765009855651bd1d8015611c6f6f25092
Gkamashidze <gqamashidze51@gmail.com>
2022-12-10 16:15:29 +0400

    JS 10/12/2022

---
+++ js/src/js/script.js
@@ -0,0 +1,54 @@
+
+
+// var a;
+
+// a=5+8;
+
+// alert(a)
+
+// alert("hello world") // ეს ამოაგდებს პუპ აპ ყუთს
+// console.log("hello world") // კონსოლში გამოიტანს ინფოს
+
+// document.write("hello world") // პირდაპირ ჰტმლ ში დაგვიწერს ტექსტს
+
+// var a=5+8;
+
+// console.log("output")
+
+// console.log(a)
+
+// var myIncome, percent, sum;
+
+// myIncome = 15;
+
+// percent = 2;
+// sum = myIncome % percent;
+
+// console.log(sum)
+
+
+// number
+// var x=45;
+// var y=23;
+
+//string
+// var companyName="comm school"
+// var companyName1="Gita"
+// var companyName2="Pyne"
+
+//boolean
+// var isAlie=true;
+// var isDead=false;
+
+// null და undefined
+
+// var isUndefined;
+// console.log(undefined)
+
+// check data type
+
+// var name="Tom"
+// console.log(typeof firstName)
+
+var x=10;
+var y=x /50;
